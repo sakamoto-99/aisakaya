95337ee5297a88ec497e792ade803e0b998e85c5
sakamoto-99 <neo.sasa.4423@gmail.com>
2021-06-03 11:49:22 +0900

    デプロイのための修正2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'sakes#index'
+  root 'sakes#index'
   get "/" => 'home#top'
   # root to: 'sakes#new'
   resources :sakes, only: [:index, :new, :create]
